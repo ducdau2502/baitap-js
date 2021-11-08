12599b959e09e5ae43e51da1f94cfd4c8c361571
ducdau2502 <dauducdh2cm1@gmail.com>
2021-11-08 16:30:12 +0700

    bai 6 va bai 7

---
+++ bài 6/main.js
@@ -0,0 +1,26 @@
+// bài 1
+
+    function showMessage() {
+    alert('Hello World!');
+}
+
+// bài 2
+
+    function messageShow() {
+    document.getElementById("messageDiv").innerHTML = "Hello World!!";
+}
+
+
+// bài 3
+
+    function message() {
+    document.write("Hello World!!!")
+}
+
+// bài 4
+
+
+    function show() {
+    console.log('Hello World!');
+}
+
